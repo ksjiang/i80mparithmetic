7bdcb5637b8b5f3aecd85fbd591015133fab6c2b
siyujiang81 <siyu.jiang81@gmail.com>
2017-12-19 20:34:41 -0600

    Update and rename mpadd.a80 to mpadd.asm

--- mpadd.asm
+++ mpadd.asm
@@ -3,16 +3,15 @@ mpadd:	PUSH PSW	;save registers
 	PUSH D
 	PUSH H
 
-mpadd1:	LXI H, 0FFF0H	;do SP - 16
+mpadd1:	LXI H, 0FFF6H	;do SP - 10
 	DAD SP
 	SPHL
+	POP H		;*res
+	DCX SP
+	DCX SP
+	POP D		;*add2
 	POP B		;counter
-	INX SP
-	INX SP
-	POP D
-	INX SP
-	INX SP
-	POP H
+	MOV C, B	;dup counter
 
 mpadd2:	LDAX D		;copy addend to result block
 	MOV M, A
@@ -21,23 +20,23 @@ mpadd2:	LDAX D		;copy addend to result block
 	DCR B
 	JNZ mpadd2
 
-mpadd2:	LXI B, Y
-	LXI H, Z
-	MVI D, SL
+mpadd2:	LXI H, 0008H	;do SP + 8
+	DAD SP
+	SPHL
+	POP H		;*res
+	POP D		;*add1
 	ORA A		;reset carry flag
 
-mpadd3:	LDAX B		;main addition loop
+mpadd3:	LDAX D		;main addition loop
 	ADC M
 	MOV M, A
-	DCR D
-	JZ mpadd4
-	INX B
+	DCR C		;duped counter
+	JZ mpadd4	;all done
+	INX D
 	INX H
 	JMP mpadd3
-mpadd4:	JNC mpadd5
-	INX H
-	INR M
-mpadd5:	POP H		;restore registers
+
+mpadd4:	POP H		;restore registers
 	POP D
 	POP B
 	POP PSW
