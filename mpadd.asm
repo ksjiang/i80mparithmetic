272c866191e083b8253c7950c8510ba7df0875f9
siyujiang81 <siyu.jiang81@gmail.com>
2017-12-19 20:35:17 -0600

    Update mpadd.asm

--- mpadd.asm
+++ mpadd.asm
@@ -20,23 +20,23 @@ mpadd2:	LDAX D		;copy addend to result block
 	DCR B
 	JNZ mpadd2
 
-mpadd2:	LXI H, 0008H	;do SP + 8
+mpadd3:	LXI H, 0008H	;do SP + 8
 	DAD SP
 	SPHL
 	POP H		;*res
 	POP D		;*add1
 	ORA A		;reset carry flag
 
-mpadd3:	LDAX D		;main addition loop
+mpadd4:	LDAX D		;main addition loop
 	ADC M
 	MOV M, A
 	DCR C		;duped counter
-	JZ mpadd4	;all done
+	JZ mpadd5	;all done
 	INX D
 	INX H
-	JMP mpadd3
+	JMP mpadd4
 
-mpadd4:	POP H		;restore registers
+mpadd5:	POP H		;restore registers
 	POP D
 	POP B
 	POP PSW
